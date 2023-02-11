b71f760dcf87d9f0fc9e04e6971948ef180d5f76
Kristin Schlossman <Kristin.Schlossman@gmail.com>
2023-02-11 08:12:55 -0800

    work through some problems in the funcitons.js. Comments for self that may resolve later.

--- functions.js
+++ functions.js
@@ -4,13 +4,24 @@
 // Use console.log() to see the return value in the console.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
-function greeting();
+function greeting() {
+    return  `hey there`
+}
+console.log(greeting());
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
-function customGreeting();
+function customGreeting(name) {
+    return `Hi, how are you ${name}`
+}
+console.log(customGreeting("Annabelle"));
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-function greetPerson();
+function greetPerson(firstName, middleName, lastName) {
+    return `thanks for signing up ${firstName, middleName, lastName}!`
+}
+console.log(greetPerson("Michelle", "Lee", "O'Sullivan"));
+// QUESTION FOR PROFESSORS: this seems inelligant. Is there a better way to do this, perhaps with a variable for full name?
+//Keep working on this, it only prints last name rn for some reason. 
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
@@ -18,7 +29,7 @@ function greetPerson();
 
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
-function checkStock();
+function checkStock()
 checkStock(4, "Coffee");
 // => "Coffee is stocked"
 
